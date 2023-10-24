42d542f47e9701b4823db748c13ba9af2ef61e0b
0xSamWitch <84033732+0xSamWitch@users.noreply.github.com>
2023-10-24 20:37:39 +0000

    Deploying to gh-pages from @ PaintSwap/fantom-sonic-frontend@3852ad353c3703d1d7125057b945da1f105f172a 🚀

---
+++ _next/static/KqqsLycYol22gy0YR0GGD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/92-7c56ef31383e8e9d.js","static/css/fc26aeb2c5c24854.css","static/chunks/pages/index-437afc057651ea7b.js"],"/_error":["static/chunks/pages/_error-a59e2db023c5e431.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
