d5bcfc096951d7d56892684c14a2776c374c1379
paintoshi <88572775+paintoshi@users.noreply.github.com>
2024-09-06 14:21:50 +0000

    Deploying to gh-pages from @ PaintSwap/fantom-sonic-frontend@c949a5a42ff4d6fef9ac9bea029ab011f9e3ce00 🚀

---
+++ _next/static/5ahKfVG4sVetrnmY3lUcw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/92-81412e278803bb76.js","static/css/fa429449de0763b4.css","static/chunks/pages/index-4861b92824af2aa9.js"],"/_error":["static/chunks/pages/_error-a59e2db023c5e431.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
