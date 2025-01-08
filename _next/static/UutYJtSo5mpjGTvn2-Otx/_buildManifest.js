bb44d2c23dfe2966993e39a6eb10095df10b3f03
paintoshi <88572775+paintoshi@users.noreply.github.com>
2025-01-08 09:32:10 +0000

    Deploying to gh-pages from @ PaintSwap/speedchecker-frontend@7efaa9b29ada3022d9616ce844d15d3a55e52cd0 🚀

---
+++ _next/static/UutYJtSo5mpjGTvn2-Otx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/255-c42e0d3e77b9e02a.js","static/css/057c9934984c3843.css","static/chunks/pages/index-1e265c5fc991b440.js"],"/_error":["static/chunks/pages/_error-a59e2db023c5e431.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/ghsA9iJTvPGb1ZToh83Fn/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/255-c42e0d3e77b9e02a.js","static/css/057c9934984c3843.css","static/chunks/pages/index-a6f5ef3259434b0b.js"],"/_error":["static/chunks/pages/_error-a59e2db023c5e431.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
